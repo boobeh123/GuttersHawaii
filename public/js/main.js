8b91151a0e79cba65541aa8cdc1b811c5429ee5f
Bobby <boobeh321@gmail.com>
2025-12-25 11:24:51 -1000

    Dynamic footer

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     validateForms();
     initGallery();
+
+    const yearSpan = document.querySelector('#copyright');
+    if (yearSpan) {
+        yearSpan.innerHTML = `&copy; ${new Date().getFullYear()} Gutters Hawaii LLC. All rights reserved.`;
+    }
 });
 
 async function validateForms() {
